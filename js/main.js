eef2b3d5d41150367e7b248badc56bc78693b7ca
webQbe <webcubesolutionslk@gmail.com>
2024-10-21 20:18:52 +0530

    Added listener for dragend event of fill element

--- js/main.js
+++ js/main.js
@@ -5,5 +5,8 @@ const fill = document.querySelector('.fill');
 // Select empty boxes
 const empties = document.querySelectorAll('.empty');
 
-// Listen for drags from fill
+// Listen for drags starts
 fill.addEventListener('dragstart', dragStart);
+
+// Listen for drags ends
+fill.addEventListener('dragend', dragEnd);
