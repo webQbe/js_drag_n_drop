3b15f18e48fe4cce2f98be288cfdee964b66abdc
webQbe <webcubesolutionslk@gmail.com>
2024-10-21 20:16:46 +0530

    Added listener for dragstart events in fill element

--- js/main.js
+++ js/main.js
@@ -4,3 +4,6 @@ const fill = document.querySelector('.fill');
 
 // Select empty boxes
 const empties = document.querySelectorAll('.empty');
+
+// Listen for drags from fill
+fill.addEventListener('dragstart', dragStart);
