fd6a2b7773f98a77779584adeabb420ee381131b
webQbe <webcubesolutionslk@gmail.com>
2024-10-21 21:02:40 +0530

    Added Event Listeners for Empty Boxes

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ const fill = document.querySelector('.fill');
 // Select empty boxes
 const empties = document.querySelectorAll('.empty');
 
+
+
+// Listen for Fill 
+
 // Listen for drags starts
 fill.addEventListener('dragstart', dragStart);
 
@@ -21,6 +25,26 @@ function dragStart(){
 
 function dragEnd(){
 
-    this.className = 'fill'; // This returns image back to .fill on mouse release
+    this.className = 'fill'; // This returns image back to .fill box on mouse release
+
+}
+
+
+// Listen for Empty Boxes
+
+// Loop through empties array & call drag events
+for(const empty of empties){
+
+    // Listen for when image is dragged over 
+    empty.addEventListener('dragover', dragOver);
+
+    // Listen for when image is entered
+    empty.addEventListener('dragenter', dragEnter);
+
+    // Listen for when image is left 
+    empty.addEventListener('dragleave', dragLeave);
+
+    // Listen for when image is dropped
+    empty.addEventListener('drop', dragDrop);
 
 }
